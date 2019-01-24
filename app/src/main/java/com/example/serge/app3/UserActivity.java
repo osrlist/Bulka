5d98cd3221a8a963fee27e0eaf117cbe0898069f
sbadin <sbadin@diasoft.ru>
2019-01-24 08:19:39 +0700

    Пока еще не рабочие состояние.
    попытка перйти на CursorLoader

--- app/src/main/java/com/example/serge/app3/UserActivity.java
+++ app/src/main/java/com/example/serge/app3/UserActivity.java
@@ -1,17 +1,30 @@
 package com.example.serge.app3;
 
+import java.util.concurrent.TimeUnit;
+
+import java.util.concurrent.TimeUnit;
+
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
-public class UserActivity extends AppCompatActivity {
+
+public class UserActivity extends FragmentActivity implements LoaderCallbacks<Cursor> {
 
     private DBHelper dbHelper;
     private ListView lvData;
-    private Cursor cursor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SimpleCursorAdapter scAdapter;
@@ -21,21 +34,22 @@ public class UserActivity extends AppCompatActivity {
 
         dbHelper =  new DBHelper(this);
         SQLiteDatabase database = dbHelper.getWritableDatabase();
-        cursor = database.rawQuery("Select UserID as _id" +
-                ", FirstName" +
-                ", MiddleName" +
-                ", LastName" +
-                " from tUser" +
-                " order by LastName, FirstName", null);
-        startManagingCursor(cursor);
         // формируем столбцы сопоставления
+
         String[] from = new String[] { "FirstName", "MiddleName", "LastName" };
         int[] to = new int[] { R.id.tvItemFirstName, R.id.tvItemMiddleName, R.id.tvItemLastName };
 
         // создааем адаптер и настраиваем список
-        scAdapter = new SimpleCursorAdapter(this, R.layout.item_user, cursor, from, to);
+        scAdapter = new SimpleCursorAdapter(this, R.layout.item_user, null, from, to, 0);
         lvData = (ListView) findViewById(R.id.lvUser);
         lvData.setAdapter(scAdapter);
+        // создаем лоадер для чтения данных
+        getSupportLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
+        return new MyCursorLoader(this );
     }
 
     @Override
@@ -48,5 +62,47 @@ public class UserActivity extends AppCompatActivity {
     public void onLowMemory() {
         super.onLowMemory();
         dbHelper.close();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle bndl) {
+        return new MyCursorLoader(this, db);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        scAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+    static class MyCursorLoader extends CursorLoader {
+
+        private DBHelper dbHelper;
+
+        public MyCursorLoader(Context context, DBHelper dbHelper) {
+            super(context);
+            this.dbHelper = dbHelper;
+        }
+        @Override
+        public Cursor loadInBackground() {
+            Cursor cursor;
+            SQLiteDatabase database = dbHelper.getWritableDatabase();
+            cursor = database.rawQuery("Select UserID as _id" +
+                    ", FirstName" +
+                    ", MiddleName" +
+                    ", LastName" +
+                    " from tUser" +
+                    " order by LastName, FirstName", null);
+
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return cursor;
+        }
+
     }
 }
